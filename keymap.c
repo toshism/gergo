cc8da73b455f91c5db7fd8098e8d3d21847519a2
tosh <toshism@gmail.com>
2019-10-22 16:01:00 +0200

    eh, a lot of refactoring

--- keymap.c
+++ keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                         ,-------------------------------------------.
  * | L1/ESC |   Q  |   W  |   E  |   R  |   T  |                         |   Y  |   U  |   I  |   O  |   P  |  | \   |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |ALT/D |CTL/F |   G  | RMB  |           |      |   H  |CTL/J |ALT/K |   L  | ;  : |  ' "   |
+ * | TEN/BS |   A  |   S  |ALT/D |CTL/F |   G  | RMB  |           |      |   H  |CTL/J |ALT/K |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | LMB  |           |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -43,15 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      | '     |                      |       |      |
  *                                 `--------------'                      `--------------'
  */
-[BASE] = LAYOUT_GERGO(
-LT(NUMB, KC_ESC),       KC_Q,  KC_W,   KC_E,           KC_R,         KC_T,                KC_Y,     KC_U, KC_I,         KC_O,         KC_P,   KC_PIPE,
-MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   LALT_T(KC_D),   LCTL_T(KC_F), KC_G, TT(TEN),       KC_TRNS,  KC_H, RCTL_T(KC_J), LALT_T(KC_K), KC_L,   KC_SCLN, KC_QUOT,
-KC_RSFT,                KC_Z,  KC_X,   KC_C,           KC_V,         KC_B, KC_BTN1,       KC_BSPC,  KC_N, KC_M,         KC_COMM,      KC_DOT, KC_SLSH, KC_MINS,
+[BASE] = LAYOUT_gergo(
+LT(NUMB, KC_TAB),  KC_Q,  KC_W,   KC_E,           KC_R,         KC_T,                                             KC_Y, KC_U,         KC_I,         KC_O,   KC_P,    KC_PIPE,
+LT(TEN, KC_BSPC),  KC_A,  KC_S,   LALT_T(KC_D),   LCTL_T(KC_F), KC_G, TT(TEN),                          KC_TRNS,  KC_H, RCTL_T(KC_J), LALT_T(KC_K), KC_L,   KC_SCLN, KC_QUOT,
+KC_RSFT,           KC_Z,  KC_X,   KC_C,           KC_V,         KC_B, KC_BTN1, KC_TRNS,       KC_BSPC,  KC_BSPC,  KC_N, KC_M,         KC_COMM,      KC_DOT, KC_SLSH, KC_MINS,
 
-         KC_RSFT, LT(SYMB, KC_BSPC),                                                                                           LT(SYMB, KC_SPC), KC_RSFT,
-
-                                                           KC_TRNS,                       KC_TRNS,
-                                   LT(NUMB, KC_ESC), LCA(KC_QUOTE),                       LT(WIN, KC_TAB), LT(NUMB, KC_ENT)),
+           KC_RSFT, LT(SYMB, KC_BSPC), LT(NUMB, KC_ESC), KC_LGUI,                                             KC_TAB, LT(NUMB, KC_ENT), LT(SYMB, KC_SPC), KC_RSFT),
 /* Keymap 1: Symbols layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
@@ -72,14 +69,12 @@ KC_RSFT,                KC_Z,  KC_X,   KC_C,           KC_V,         KC_B, KC_BT
  *                                 |      |       |                     |       |      |
  *                                 `--------------'                     `--------------'
  */
-[SYMB] = LAYOUT_GERGO(
-KC_TRNS, KC_EXLM, KC_AT,  KC_LCBR,KC_RCBR, KC_PIPE,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
-KC_TRNS, KC_HASH, KC_DLR, KC_LPRN,KC_RPRN, KC_GRV,  KC_TRNS,       KC_TRNS,  KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_UNDS,
-KC_TRNS, KC_PERC, KC_CIRC,KC_LBRC,KC_RBRC, KC_TILD, KC_TRNS,       KC_TRNS,  KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
+[SYMB] = LAYOUT_gergo(
+KC_TRNS, KC_EXLM, KC_AT,  KC_LCBR,KC_RCBR, KC_PIPE,                                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
+KC_TRNS, KC_HASH, KC_DLR, KC_LPRN,KC_RPRN, KC_GRV,  KC_TRNS,                         KC_TRNS, KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_UNDS,
+KC_TRNS, KC_PERC, KC_CIRC,KC_LBRC,KC_RBRC, KC_TILD, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
 
-                                                  KC_TRNS, KC_TRNS,       KC_PGUP, KC_DEL,
-                                                           KC_TRNS,       KC_TRNS,
-                                             	  KC_SCLN, KC_EQL,        KC_EQL, KC_SCLN),
+                                   KC_TRNS, KC_TRNS, KC_SCLN, KC_EQL,       KC_EQL, KC_SCLN, KC_PGUP, KC_DEL),
 /* Keymap 2: Pad/Function layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
@@ -100,14 +95,12 @@ KC_TRNS, KC_PERC, KC_CIRC,KC_LBRC,KC_RBRC, KC_TILD, KC_TRNS,       KC_TRNS,  KC_
  *                                 |      |       |                     |       |      |
  *                                 `--------------'                     `--------------'
  */
-[NUMB] = LAYOUT_GERGO(
-KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
-KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,        KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
+[NUMB] = LAYOUT_gergo(
+KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                              KC_6,          KC_7,          KC_8,        KC_9,          KC_0,    KC_TRNS,
+KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_TRNS,  KC_LEFT,       KC_DOWN,       KC_UP,       KC_RGHT,       KC_VOLD, KC_VOLU,
+KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_TRNS,       KC_TRNS, KC_TRNS,  LALT(KC_LEFT), LALT(KC_DOWN), LALT(KC_UP), LALT(KC_RGHT), KC_MS_R, KC_MPLY,
 
-                                                  KC_TRNS, KC_TRNS,       KC_PGUP, KC_TRNS,
-                                                           KC_TRNS,       KC_TRNS,
-                                                  KC_TRNS, KC_TRNS,       KC_MS_BTN1, KC_MS_BTN2),
+                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_PGUP, KC_TRNS, KC_MS_BTN1, KC_MS_BTN2),
 
 
 /* Keymap template
@@ -130,7 +123,7 @@ KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,        KC_TRNS,  K
  *                                 |      |       |       |       |      |
  *                                 `--------------'       `--------------'
  */
-[WIN] = LAYOUT_GERGO(
+[WIN] = LAYOUT_gergo(
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), LALT(KC_5), KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
@@ -140,14 +133,12 @@ KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   
                                                   KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS),
 
 
-[TEN] = LAYOUT_GERGO(
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,         KC_TRNS,  KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,         KC_TRNS,  KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
+[TEN] = LAYOUT_gergo(
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                      KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
 
-                                                  KC_TRNS, KC_TRNS,       KC_0, KC_DOT,
-                                                           KC_TRNS,       KC_TRNS,
-                                             	  KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS)
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_0, KC_DOT)
 };
 
 // Runs just one time when the keyboard initializes.
